8e9d4dd21e1900739b6441ab299cff6bfab0a2ad
ysak-y <yoshiaki.0614@gmail.com>
2017-04-02 14:47:47 +0900

    Update js

--- static/js/main.js
+++ static/js/main.js
@@ -18,14 +18,65 @@ recognition.onstart = function(event) {
 }
 
 recognition.onend = function(event) {
-  
+  console.log('recognition end');
+  var uttr = new SpeechSynthesisUtterance('聞くのをやめます');
+  SpeechSynthesis.speak(uttr);
+  if (isListening == false) {
+    restart();
+  }
 }
 
 recognition.onerror = function(event) {
-
+  console.log('recognition end');
+  var uttr = new SpeechSynthesisUtterance('エラーが発生しました');
+  SpeechSynthesis.speak(uttr);
+  if (isListening == false) {
+    restart();
+  }
 }
 
 recognition.onresult = function(event) {
+  console.log('recognition onresult');
+  var len = event.results.length;
+  var verb = event.results[lem - 1][0].transcript;
+
+  if (isListening == true) {
+    var client = new ApiAi.ApiAiClient(
+        {
+          accessToken: ,
+          lang: 'ja'
+        }
+    );
+    recognition.stop();
+    client.textRequest(verb).then(
+      function(serverResponse) {
+        console.log(serverResponse);
+        handlingResult(serverResponse);
+        isListening = false;
+        recognition.start();
+      }
+    )
+    .catch(
+      function(serverError) {
+        console.log(serverError);
+        isListening = false;
+        recognition.start();
+      }
+    );
+  } else {
+    if (verb == 'バトラー') {
+      isListening = true;
+      console.log('start listening');
+    } else {
+      console.log(verb)
+    }
+  }
 
 }
 
+function restart() {
+  recognition.stop();
+  recognition.start();
+}
+
+window.onload = recognition.start();
